d4b0073a45a1bba8a41c745d33ae31e0d6064916
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2025-07-17 21:29:51 -0400

    Add files via upload

---
+++ PocketVehicles/__init__.py
@@ -0,0 +1,28 @@
+from mods_base import get_pc, keybind, build_mod
+from unrealsdk import find_class 
+from unrealsdk.unreal import IGNORE_STRUCT
+
+gameplay_statics = find_class("GameplayStatics").ClassDefaultObject
+
+@keybind("Spawn Vehicle")
+def spawn_vehicle() -> None:
+    if not get_pc().Pawn or not get_pc().Pawn.PlayerBalanceState:
+        return
+
+    ride = gameplay_statics.SpawnObject(find_class("CatchARide"), get_pc())
+    platform = gameplay_statics.SpawnObject(find_class("CatchARidePlatform"), get_pc())
+    ride.CatchARide_Platform1 = platform
+
+    player_loc = get_pc().Pawn.K2_GetActorLocation()
+    player_rot = get_pc().Pawn.K2_GetActorRotation()
+    
+    platform.PlatformSafeZone.K2_SetRelativeLocationAndRotation(player_loc, player_rot, False, IGNORE_STRUCT, True)
+    platform.PlatformSmallVehicleSafeZone1.K2_SetRelativeLocationAndRotation(player_loc, player_rot, False, IGNORE_STRUCT, True)
+    platform.SmallVehicleSpawnSocket1.K2_SetRelativeLocationAndRotation(player_loc, player_rot, False, IGNORE_STRUCT, True)
+    
+    veh_index = get_pc().CurrentSavegame.VehicleLastLoadoutIndex
+    car = get_pc().VehicleSpawnerComponent.VehicleLoadouts[veh_index]
+
+    get_pc().SpawnVehicleFromConfig(get_pc().Pawn.PlayerBalanceState.GetExperienceLevel(),car,ride)
+    
+build_mod()
