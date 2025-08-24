c6f2e0b3a9703a31c2d249c09cac835e213f48bc
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2025-08-24 15:29:49 -0400

    Add files via upload

--- PhaseflareFix/__init__.py
+++ PhaseflareFix/__init__.py
@@ -6,17 +6,51 @@ from unrealsdk.unreal import UObject, WrappedStruct
 
 orb_states = [3,4]
 
+collision_channels = ['WorldStatic',
+                      'WorldDynamic',
+                      'Visibility',
+                      'Camera',
+                      'PhysicsBody',
+                      'Vehicle',
+                      'Destructible',
+                      'EngineTraceChannel1',
+                      'EngineTraceChannel2',
+                      'EngineTraceChannel3',
+                      'EngineTraceChannel4',
+                      'EngineTraceChannel5',
+                      'EngineTraceChannel6',
+                      'GameTraceChannel1',
+                      'GameTraceChannel2',
+                      'GameTraceChannel3',
+                      'GameTraceChannel4',
+                      'GameTraceChannel5',
+                      'GameTraceChannel6',
+                      'GameTraceChannel7',
+                      'GameTraceChannel8',
+                      'GameTraceChannel9',
+                      'GameTraceChannel10',
+                      'GameTraceChannel11',
+                      'GameTraceChannel12',
+                      'GameTraceChannel13',
+                      'GameTraceChannel14',
+                      'GameTraceChannel15',
+                      'GameTraceChannel16',
+                      'GameTraceChannel17',
+                      'GameTraceChannel18',
+                      ]
+
 @hook(
     "/Game/PlayerCharacters/SirenBrawler/_DLC/Ixora/Phasetrance/Projectiles/Proj_Siren_DLCSkill_WalkingPotato_Base.Proj_Siren_DLCSkill_WalkingPotato_Base_C:SetOrbState",
     Type.PRE
     )
 def PhasePhlarePhix(obj: UObject, args: WrappedStruct, *_) -> None:
+    response = obj.Sphere.BodyInstance.CollisionResponses.ResponseToChannels
     if args.NewState in orb_states:
-        obj.Sphere.BodyInstance.CollisionResponses.ResponseToChannels.WorldStatic = 0
-        obj.Sphere.BodyInstance.CollisionResponses.ResponseToChannels.WorldDynamic = 0
+        for channel in collision_channels:
+            setattr(response, channel, 0)
     else:
-        obj.Sphere.BodyInstance.CollisionResponses.ResponseToChannels.WorldStatic = 1
-        obj.Sphere.BodyInstance.CollisionResponses.ResponseToChannels.WorldDynamic = 1
+        for channel in collision_channels:
+            setattr(response, channel, 1)
     return
 
  
